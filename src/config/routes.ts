12f2e07f0af2ae346603e2e4a145b51582e1a15d
Setgey <serqei_gudzenko@mail.ru>
2021-01-07 01:59:52 +0200

    remove app passport

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,9 +1,6 @@
-import { Express } from "express";
-// import { ScooterRouter } from '../api/v1/scooters'
-import { citiesRouter } from "../api/v1/cities";
-// import { scooterRouter } from '../api/v1/scooters'
+import { Express } from 'express'
+import { citiesRouter } from '../api/v1/cities'
 
 export const setupRoutesV1 = (app: Express): void => {
-  app.use("/v1/cities", citiesRouter);
-  //   app.use('/v1/scooters', scooterRouter)
-};
+  app.use('/v1/cities', citiesRouter)
+}
