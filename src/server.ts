9f3f413512631a09945ff1823b911fd1070e4db2
Setgey <serqei_gudzenko@mail.ru>
2021-01-07 02:28:24 +0200

    Test port

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import logger from './util/logger'
 
 const server = app.listen(3000, (): void => {
   logger.info(
-    `App is running at http://localhost:${app.get('port') || 80} in ${app.get(
+    `App is running at http://localhost:${server.address().port} in ${app.get(
       'env',
     )} mode`,
   )
