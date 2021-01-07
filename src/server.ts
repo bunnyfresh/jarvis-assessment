eb56810849ce37beb1a8056aac71bc7882eab675
Setgey <serqei_gudzenko@mail.ru>
2021-01-07 03:32:22 +0200

    f

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,6 @@ import app from './app'
 import logger from './util/logger'
 import { APP_PORT } from './config/settings'
 
-const server = app.listen(APP_PORT, '81.17.137.76', (): void => {
-  logger.info(
-    `App is running at http://localhost:${server.address()} in ${app.get(
-      'env',
-    )} mode`,
-  )
-})
+const server = app.listen(APP_PORT, '81.17.137.76')
 
 export default server
