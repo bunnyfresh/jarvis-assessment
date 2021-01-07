6d0470ddbdd0969c8b6c8e5c828d0729420d5440
Setgey <serqei_gudzenko@mail.ru>
2021-01-07 03:27:57 +0200

    f

--- src/app.ts
+++ src/app.ts
@@ -10,15 +10,12 @@ mongoose.set('useNewUrlParser', true)
 mongoose.set('useCreateIndex', true)
 mongoose.set('useUnifiedTopology', true)
 
-mongoose
-  .connect(MONGO_URI)
-  .catch((err): void => {
-    logger.error(
-      'MongoDB connection error. Please make sure MongoDB is running. ' + err,
-    )
-    process.exit(1)
-  })
-  .then((e) => console.log('!!!!!!!!!!!', e))
+mongoose.connect(MONGO_URI).catch((err): void => {
+  logger.error(
+    'MongoDB connection error. Please make sure MongoDB is running. ' + err,
+  )
+  process.exit(1)
+})
 
 const app = express()
 
