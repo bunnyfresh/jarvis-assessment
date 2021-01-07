1eaa80ef9d95492e6b412608aae4e3f3f202f181
Setgey <serqei_gudzenko@mail.ru>
2021-01-07 03:20:32 +0200

    test

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,31 @@
-import express from "express";
-import mongoose from "mongoose";
-import { MONGO_URI } from "./config/secrets";
-import logger from "./util/logger";
-import { setupExpress } from "./config/express";
-import { handleMissing, handleErrors } from "./middleware";
-import { setupRoutesV1 } from "./config/routes";
+import express from 'express'
+import mongoose from 'mongoose'
+import { MONGO_URI } from './config/secrets'
+import logger from './util/logger'
+import { setupExpress } from './config/express'
+import { handleMissing, handleErrors } from './middleware'
+import { setupRoutesV1 } from './config/routes'
 
-mongoose.set("useNewUrlParser", true);
-mongoose.set("useCreateIndex", true);
-mongoose.set("useUnifiedTopology", true);
+mongoose.set('useNewUrlParser', true)
+mongoose.set('useCreateIndex', true)
+mongoose.set('useUnifiedTopology', true)
 
-mongoose.connect(MONGO_URI).catch((err): void => {
+mongoose
+  .connect(MONGO_URI)
+  .catch((err): void => {
     logger.error(
-        "MongoDB connection error. Please make sure MongoDB is running. " + err
-    );
-    process.exit(1);
-});
+      'MongoDB connection error. Please make sure MongoDB is running. ' + err,
+    )
+    process.exit(1)
+  })
+  .then((e) => console.log('!!!!!!!!!!!', e))
 
-const app = express();
+const app = express()
 
-setupExpress(app);
-setupRoutesV1(app);
+setupExpress(app)
+setupRoutesV1(app)
 
-app.use(handleMissing);
-app.use(handleErrors);
+app.use(handleMissing)
+app.use(handleErrors)
 
-export default app;
+export default app
