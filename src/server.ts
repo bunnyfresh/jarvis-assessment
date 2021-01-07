58cc605e33637c15812b7964b46415495841f05b
Setgey <serqei_gudzenko@mail.ru>
2021-01-07 02:34:28 +0200

    Gix p

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import app from './app'
 import logger from './util/logger'
 
-const server = app.listen(3000, (): void => {
+const server = app.listen(3000,, '0.0.0.0', (): void => {
   logger.info(
     `App is running at http://localhost:${server.address()} in ${app.get(
       'env',
