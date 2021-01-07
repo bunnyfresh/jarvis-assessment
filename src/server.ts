ecf052900b9350086ed2bb62b6b00182ccfdb4b3
Setgey <serqei_gudzenko@mail.ru>
2021-01-07 02:10:57 +0200

    fix [-[p---\

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import app from './app'
 import logger from './util/logger'
 
-const server = app.listen(80, (): void => {
+const server = app.listen(3000, (): void => {
   logger.info(
     `App is running at http://localhost:${app.get('port') || 80} in ${app.get(
       'env',
